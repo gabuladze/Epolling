964aec968c2941525fb41f812476df939d89bd84
Giorgi Abuladze <g.abuladze@live.com>
2017-04-30 19:00:27 +0400

    Added using statement for System.Xml

--- Epolling/Form1.cs
+++ Epolling/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Epolling
 {
@@ -15,6 +16,13 @@ namespace Epolling
         public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            // Fetch textbox value
+            string userId = IdTextBox.Text;
+
         }
     }
 }
