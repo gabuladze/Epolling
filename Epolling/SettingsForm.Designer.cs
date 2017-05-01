9aaf071b2ef9e714b6997a130d6eeaa2c82b7683
Giorgi Abuladze <g.abuladze@live.com>
2017-05-01 23:29:12 +0400

    Added a listbox for displaying candidates

--- Epolling/SettingsForm.Designer.cs
+++ Epolling/SettingsForm.Designer.cs
@@ -28,11 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.candidatesListLabel = new System.Windows.Forms.Label();
+            this.candidatesListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // candidatesListLabel
+            // 
+            this.candidatesListLabel.AutoSize = true;
+            this.candidatesListLabel.Location = new System.Drawing.Point(13, 13);
+            this.candidatesListLabel.Name = "candidatesListLabel";
+            this.candidatesListLabel.Size = new System.Drawing.Size(107, 13);
+            this.candidatesListLabel.TabIndex = 0;
+            this.candidatesListLabel.Text = "Edit list of candidates";
+            // 
+            // candidatesListBox
+            // 
+            this.candidatesListBox.FormattingEnabled = true;
+            this.candidatesListBox.Location = new System.Drawing.Point(16, 50);
+            this.candidatesListBox.Name = "candidatesListBox";
+            this.candidatesListBox.Size = new System.Drawing.Size(200, 342);
+            this.candidatesListBox.TabIndex = 3;
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 427);
+            this.Controls.Add(this.candidatesListBox);
+            this.Controls.Add(this.candidatesListLabel);
+            this.Name = "SettingsForm";
+            this.ShowIcon = false;
             this.Text = "SettingsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label candidatesListLabel;
+        private System.Windows.Forms.ListBox candidatesListBox;
     }
 }
